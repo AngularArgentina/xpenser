9b35553bab2dbdbb44c62ab4f4396600183961c3
fivunlm <lopez.fernando.damian@gmail.com>
2017-09-05 09:09:40 -0300

    Se agregan los componentes para la lista de gastos y el formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ExpenseListComponent } from './expense-list/expense-list.component';
+import { ExpenseFormComponent } from './expense-form/expense-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpenseListComponent,
+    ExpenseFormComponent
   ],
   imports: [
     BrowserModule,
